ccefe5c50857028f63fa74f01f93913766341de3
Ryan Coupe <rmc72@dhe.duke.edu>
2026-09-01 16:10:39 -0400

    Fix contractor light mode contrast

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "turnly-contractor-pwa-v20260901-sales-platform";
+const CACHE_NAME = "turnly-contractor-pwa-v20260901-contractor-light-contrast";
 
 const PRECACHE_URLS = [
   "/",
